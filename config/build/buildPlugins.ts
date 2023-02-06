355593a7876e64dee6450d8103fd33e48ab5240d
Igor Kiselev <igor.kiselyov.igor@yandex.ru>
2023-02-06 21:24:43 +0300

    add eslint, stylelint

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,24 +1,24 @@
-import { BuildOptions } from "./types/config";
-import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
 export function buildPlugins({
-  isDev,
-  paths,
+    isDev,
+    paths,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HTMLWebpackPlugin({
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
-    }),
-    new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ];
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ];
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,3 @@
-import { ThemeSwitcher } from "./ui/ThemeSwitcher/ThemeSwitcher";
+import { ThemeSwitcher } from './ui/ThemeSwitcher/ThemeSwitcher';
 
 export { ThemeSwitcher };
