f0f1e5a246007d4f4b32d655488c543b25be5a7d
Igor Kiselev <igor.kiselyov.igor@yandex.ru>
2023-02-06 14:14:10 +0300

    add i18next, add babel

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({
+  isDev,
   paths,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -15,5 +16,9 @@ export function buildPlugins({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 }
