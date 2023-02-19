bec68186f4e4ebd19eedb8633e5c967c5d2f7d6c
Igor Kiselev <igor.kiselyov.igor@yandex.ru>
2023-02-19 19:10:54 +0300

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: "tsconfig.json",
         tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
         ecmaVersion: "latest",
         ecmaFeatures: {
             jsx: true,
